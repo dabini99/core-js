d418ed4aa269a57f080e7daccdaed7af38b73c2e
dabini99 <djssk8475@gmail.com>
2024-06-20 17:48:52 +0900

    버블버블

--- client/lib/dom/css.js
+++ client/lib/dom/css.js
@@ -37,7 +37,7 @@ function addClass(node,...className){
   //   throw new TypeError('addClass 함수의 두 번째 인수는 문자 타입 이어야 합니다.');
   // }
   // node.classList.add(className);
-}
+// }
 
 
 function removeClass(node,className){
