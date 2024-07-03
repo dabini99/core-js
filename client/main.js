0ba1dcb444c6472ef075682d565db6a578501f53
dabini99 <djssk8475@gmail.com>
2024-07-03 11:15:35 +0900

    섀도우돔

--- client/main.js
+++ client/main.js
@@ -1,7 +1,20 @@
+
+
+
+
+
 class Button extends HTMLElement {
 
   constructor(){
     super();
+
+    // c-button의 쉐도우 돔을 열어줘.
+    this.attachShadow({mode:'open'});
+
+    // 그리고 그 안에(shadowRoot) 내가 원하는 태그 집어 넣을거야 
+    this.shadowRoot.innerHTML = `
+      <button>hello</button>
+    `
   }
 
   connectedCallback(){
@@ -12,8 +25,11 @@ class Button extends HTMLElement {
 
   }
 
-
 }
 
 
+
 customElements.define('c-button',Button);
+
+
+console.log( document.querySelector('c-button').shadowRoot.querySelector('button') );
