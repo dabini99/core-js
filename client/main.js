ae126bdd7d29d4123bc3b840166a12ff38607516
dabini99 <djssk8475@gmail.com>
2024-07-03 10:36:53 +0900

    커스텀마이즈드 빌트인 엘리먼트

--- client/main.js
+++ client/main.js
@@ -2,44 +2,18 @@ class Button extends HTMLElement {
 
   constructor(){
     super();
-
-    this.button = document.querySelector('button');
-
   }
 
   connectedCallback(){
-    this._render();
+
   }
 
   disconnectedCallback(){
 
   }
 
-  static get observedAttributes(){
-    return ['id'];
-  }
-
-  attributeChangedCallback(name,oldValue,newValue){
-    if(oldValue !== newValue){
-      this._render()
-    }
-  }
-
-  _render(){
-    this.button.textContent = this.id;
-  }
 
 }
 
 
 customElements.define('c-button',Button);
-
-
-
-const c = document.querySelector('c-button');
-
-let count = 0;
-
-c.addEventListener('click',()=>{
-  c.setAttribute('id',++count)
-})
